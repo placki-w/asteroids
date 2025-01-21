6d5c28a20382eef1f4203d7a03ac80fed92d7e81
placki-w <placki@gmail.com>
2025-01-21 14:18:16 -0500

    Basic Asteroids game complete

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # boot.dev pygame example of asteroids for some random reason
 
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -11,16 +15,46 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    #Groups for easier tracking of game objects
+    shots = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     #Player Object
     player = Player(SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     #Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
+
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill([0,0,0])
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPX
