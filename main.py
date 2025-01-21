87850c4099e674740ee8e4d49ceb9ea367200c3b
placki-w <placki@gmail.com>
2025-01-21 10:37:04 -0500

    Now we have a triangle shaped player object

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+# boot.dev pygame example of asteroids for some random reason
+
 import pygame
 from constants import *
+from player import Player
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #Player Object
+    player = Player(SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
+
     #Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
         screen.fill([0,0,0])
-        pygame.display.flip()    
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPX
+        dt = clock.tick(60) / 1000
 
 
 
